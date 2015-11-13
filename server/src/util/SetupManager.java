89df8efa8a1f0765cb48c20b3607cf7da02a6520
Michael Shibo Weng <mcw918@hotmail.com>
2015-11-13 15:41:25 -0800

    updated gitignore file and cleaned up repository

--- server/src/util/SetupManager.java
+++
@@ -1,9 +0,0 @@
-package com.mikaaudio.server.util;
-
-public class SetupManager {
-    private static final String DEVICE_NAME = "MIKA AUDIO";
-
-    public static String getDeviceName() {
-        return DEVICE_NAME;
-    }
-}
