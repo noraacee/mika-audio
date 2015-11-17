67d7662f6ad42222e6232e9eb505206561d1a7f2
Aaron Chan <AaronFChan@gmail.com>
2015-11-16 21:12:42 -0800

    add projects

---
+++ client/app/src/main/java/com/mikaaudio/client/util/SetupManager.java
@@ -0,0 +1,9 @@
+package com.mikaaudio.client.util;
+
+public class SetupManager {
+    private static final String DEVICE_NAME = "MIKA AUDIO";
+
+    public static String getDeviceName() {
+        return DEVICE_NAME;
+    }
+}
---
+++ server/app/src/main/java/com/mikaaudio/server/util/SetupManager.java
@@ -0,0 +1,9 @@
+package com.mikaaudio.server.util;
+
+public class SetupManager {
+    private static final String DEVICE_NAME = "MIKA AUDIO";
+
+    public static String getDeviceName() {
+        return DEVICE_NAME;
+    }
+}
